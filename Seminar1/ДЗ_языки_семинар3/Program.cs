29741e38743e7425a252d0e58a5edd95935e78ee
Olga <olga.krov@gmail.com>
2023-02-18 13:57:25 +0300

    tasks for Seminar1

---
+++ Seminar1/ДЗ_языки_семинар3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает 
+// число и выдает, является ли число четным
+// (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
---
+++ Seminar1/ДЗ_языки_семинар__1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает 
+//два числа и выдаёт, какое число большее,
+//а какое меньшее
+int maxNumber = 0;
+int minNumber = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    maxNumber = firstNumber;
+    minNumber = secondNumber;
+}
+else
+{
+    maxNumber = secondNumber;
+    minNumber = firstNumber;
+}
+System.Console.WriteLine($"max =  {maxNumber} min = {minNumber}");
